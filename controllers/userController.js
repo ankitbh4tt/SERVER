25a8704682f1b3bcad5ec3b51d6dc92e8216c307
Ankit <brijtest09@gmail.com>
2025-05-23 00:34:39 +0530

    completeregister user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,40 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const registerUser = async (req, res, next) => {
+    try {
+        const validateData = userSchema.parse(req.body)
+        const hashedPassword = await hashPassword(validateData.password)
+        const user = await User.create({
+            username: validateData.username,
+            password: hashedPassword,
+            email: validateData.email
+        })
+        const token = await jwt.sign(user.email,process.env.JWT_SECRET)
+        console.log(token)
+        return res.json({ 'message':`Welcome ${user.username}`,'token':token })
+    } catch (err) {
+        if (err.code === 11000) {
+            const duplicateField = Object.keys(err.keyPattern)[0]; // e.g. 'username' or 'email'
+            return res.status(400).json({
+                success: false,
+                message: `${duplicateField.charAt(0).toUpperCase() + duplicateField.slice(1)} already exists`,
+            });
+        }
 
+        // ✅ Zod validation errors
+        if (Array.isArray(err.errors)) {
+            return res.status(400).json({
+                success: false,
+                errors: err.errors.map(e => e.message),
+            });
+        }
+
+        // ✅ Fallback for other errors
+        return res.status(500).json({
+            success: false,
+            message: err.message || "Something went wrong",
+        });
+    }
 }
 
 
---
+++ middleware/errorMiddleware.js
@@ -0,0 +1,16 @@
+// middleware/errorMiddleware.js
+
+const errorHandler = (err, req, res, next) => {
+  console.error(`❌ Error: ${err.message}`);
+
+  // Custom status code if set, otherwise 500
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    success: false,
+    message: err.message || 'Server Error',
+    stack: process.env.NODE_ENV === 'production' ? '🥷' : err.stack,
+  });
+};
+
+module.exports = errorHandler;
