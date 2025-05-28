afdb14271310a2b52d143f70ce20ecad24770410
ankitbh4tt <bhattankit152207@gmail.com>
2025-05-28 21:36:10 +0530

    Seperate Login and register validation schema zod

--- validators/userInputValidator.js
+++ validators/userInputValidator.js
@@ -1,6 +1,6 @@
 const {z} = require('zod')
 
-const userSchema = z.object({
+const userRegisterSchema = z.object({
   username: z.string({
     required_error: 'Username is required!', 
     invalid_type_error: 'Username must be a string', 
@@ -18,4 +18,16 @@ const userSchema = z.object({
 });
 
 
-module.exports = {userSchema}
+const userLoginSchema = z.object({
+  email:z.string({
+    required_error:"Email is required!",
+    invalid_type_error:"Email must be a string",
+  }).email("Please enter a valid email"),
+  password: z.string({
+    required_error: 'Password is required!',
+    invalid_type_error: 'Password must be a string',
+  }).min(8, 'Password must be 8 characters long.'),
+})
+
+
+module.exports = {userRegisterSchema,userLoginSchema}
